a23ca94b092b6c1015521bb4a2ee394a978c8b52
MyCode07 <erem19992015@mail.ru>
2023-05-13 21:33:58 +0300

    added product page

---
+++ src/js/utils/product.js
@@ -0,0 +1,61 @@
+import { Swiper, Navigation, Pagination } from "swiper";
+
+const slider = document.querySelector('.product .swiper');
+if (slider) {
+    new Swiper(slider, {
+        loop: true,
+        modules: [Navigation, Pagination],
+        navigation: {
+            prevEl: '.product__images-slider-prev',
+            nextEl: '.product__images-slider-next'
+        },
+        pagination: {
+            el: '.product__pagination',
+            clickable: true
+        },
+        slidesPerView: 4,
+        breakpoints: {
+            300: {
+                direction: "horizontal",
+                spaceBetween: 30,
+            },
+
+            769: {
+                spaceBetween: 16,
+                direction: "vertical",
+            },
+        }
+    })
+}
+
+const productTabs = document.querySelectorAll('.product-tab');
+const productTabsContent = document.querySelectorAll('.product-tabs__item');
+
+if (productTabs.length) {
+    productTabs.forEach(tab => {
+        tab.addEventListener('click', function () {
+            const id = tab.dataset.id;
+
+            if (!tab.closest('.product-tabs__item')) {
+                productTabs.forEach(item => {
+                    item.classList.remove('_active')
+                })
+
+                productTabsContent.forEach(content => {
+                    if (content.id == id) {
+                        content.classList.add('_active')
+                    }
+                    else {
+                        content.classList.remove('_active')
+                    }
+                })
+
+                tab.classList.add('_active')
+            }
+            else {
+                tab.classList.toggle('_active')
+                tab.closest('.product-tabs__item').classList.toggle('_active')
+            }
+        })
+    })
+}
